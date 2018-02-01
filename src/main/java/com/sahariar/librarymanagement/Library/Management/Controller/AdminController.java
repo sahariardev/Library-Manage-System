a72466b8df47f3f4d7fb7430ccce99f18067fb91
sahariardev <sahariaralamk@gmail.com>
2018-02-01 20:52:58 +0600

    Author service linked with addAuthor controller and View

--- src/main/java/com/sahariar/librarymanagement/Library/Management/Controller/AdminController.java
+++ src/main/java/com/sahariar/librarymanagement/Library/Management/Controller/AdminController.java
@@ -1,12 +1,22 @@
 package com.sahariar.librarymanagement.Library.Management.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+
+import com.sahariar.librarymanagement.Library.Management.Models.Author;
+import com.sahariar.librarymanagement.Library.Management.Service.AuthorService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+	@Autowired
+	AuthorService as;
+	
 	
 	@RequestMapping("/dashboard")
 	public String dashboard()
@@ -40,8 +50,21 @@ public class AdminController {
 	}
 	
 	@RequestMapping("/addAuthor")
-	public String addAuthor()
+	public String addAuthor(Model model)
 	{
+		model.addAttribute("isMessage", false);
+		return "admin/addauthor";
+	}
+	
+	@RequestMapping(value="/addAuthor",method=RequestMethod.POST)
+	public String addAuthorTodatabase(WebRequest request,Model model)
+	{
+		Author author=new Author();
+		author.setName(request.getParameter("name"));
+		author.setDescription(request.getParameter("description"));
+		as.addAuthor(author);
+		model.addAttribute("message", author.getName()+" added ");
+		model.addAttribute("isMessage", true);
 		return "admin/addauthor";
 	}
 	
--- src/main/java/com/sahariar/librarymanagement/Library/Management/Service/AuthorService.java
+++ src/main/java/com/sahariar/librarymanagement/Library/Management/Service/AuthorService.java
@@ -1,5 +1,20 @@
 package com.sahariar.librarymanagement.Library.Management.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sahariar.librarymanagement.Library.Management.Jpa.AuthorRepository;
+import com.sahariar.librarymanagement.Library.Management.Models.Author;
+
+@Service
 public class AuthorService {
 
+	
+	@Autowired
+	AuthorRepository ar;
+	public Author addAuthor(Author author)
+	{
+	   	Author a=ar.save(author);
+	   	return a;
+	}
 }
